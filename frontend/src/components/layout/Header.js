992689431610526f925eb2eaac70ffad38907b25
Oduro T.J Barnes <ohenesetwumasi@gmail.com>
2020-11-09 04:54:56 +0000

    Separate Navs

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -25,90 +25,170 @@ const Header = () => {
 
   return (
     <>
-      <Nav></Nav>
-      <Navbar className="main-nav">
-        <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>
-              <Image className="logo" src="/logo(1).png" alt="" rounded />
-            </Navbar.Brand>
-          </LinkContainer>
-          <Navbar.Toggle
-            className="form-control"
-            style={{ maxWidth: "50px" }}
-            aria-controls="basic-navbar-nav"
-          >
-            {" "}
-            <svg
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              className="bi bi-filter-right"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
+      <header>
+        <Nav className="secondary-nav">
+          <Container></Container>
+        </Nav>
+        <Navbar className="nav-desktop" expand="lg" collapseOnSelect>
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                <Image className="logo" src="/logo(1).png" alt="" rounded />
+              </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle
+              className="form-control"
+              style={{ maxWidth: "50px" }}
+              aria-controls="basic-navbar-nav"
             >
-              <path
-                fillRule="evenodd"
-                d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"
-              />
-            </svg>
-          </Navbar.Toggle>{" "}
-          {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-          <Nav className="ml-auto">
-            {userInfo ? (
-              <NavDropdown title={userInfo.name} id="username">
-                <LinkContainer to="/profile">
-                  <NavDropdown.Item>Profile</NavDropdown.Item>
-                </LinkContainer>
-
-                <NavDropdown.Item onClick={logOutHandler}>
-                  Logout
-                </NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <NavDropdown
-                title={<i className="icon fas fa-user"></i>}
-                id="userMenu"
+              {" "}
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                className="bi bi-filter-right"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <LinkContainer to="/signin">
-                  <NavDropdown.Item>Sign In</NavDropdown.Item>
+                <path
+                  fillRule="evenodd"
+                  d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"
+                />
+              </svg>
+            </Navbar.Toggle>{" "}
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <LinkContainer to="/cart">
+                  <Nav.Link className="nav-link">
+                    <i className="icon fas fa-shopping-cart"></i>
+                    <sup>
+                      <Badge pill variant="success">
+                        {cartItems.length !== 0 &&
+                          cartItems.reduce(
+                            (acc, item) => acc + item.quantity,
+                            0
+                          )}
+                      </Badge>
+                    </sup>
+                  </Nav.Link>
                 </LinkContainer>
 
-                <LinkContainer to="/register">
-                  <NavDropdown.Item>Register</NavDropdown.Item>
-                </LinkContainer>
-              </NavDropdown>
-            )}
-            {userInfo && userInfo.isAdmin && (
-              <NavDropdown title="Admin" id="adminMenu">
-                <LinkContainer to="/admin/users">
-                  <NavDropdown.Item>Users</NavDropdown.Item>
-                </LinkContainer>
+                {userInfo ? (
+                  <NavDropdown title={userInfo.name} id="username">
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>Profile</NavDropdown.Item>
+                    </LinkContainer>
 
-                <LinkContainer to="/admin/products">
-                  <NavDropdown.Item>Products</NavDropdown.Item>
-                </LinkContainer>
+                    <NavDropdown.Item onClick={logOutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <LinkContainer to="/signin">
+                    <Nav.Link>
+                      <i className="icon fa fa-user"></i>Sign In
+                    </Nav.Link>
+                  </LinkContainer>
+                )}
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        {/* Mobile Nav */}
+        <Navbar className="main-nav nav-mobile" collapseOnSelect>
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                <Image className="logo" src="/logo(1).png" alt="" rounded />
+              </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle
+              className="form-control"
+              style={{ maxWidth: "50px" }}
+              aria-controls="basic-navbar-nav"
+            >
+              {" "}
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                className="bi bi-filter-right"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14 10.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0 0 1h7a.5.5 0 0 0 .5-.5zm0-3a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0 0 1h11a.5.5 0 0 0 .5-.5z"
+                />
+              </svg>
+            </Navbar.Toggle>{" "}
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto">
+                {userInfo ? (
+                  <NavDropdown
+                    title={<i className="icon fas fa-user"></i>}
+                    id="username"
+                  >
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>{userInfo.name}</NavDropdown.Item>
+                    </LinkContainer>
 
-                <LinkContainer to="/admin/orders">
-                  <NavDropdown.Item>Orders</NavDropdown.Item>
+                    <NavDropdown.Item onClick={logOutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+
+                    {userInfo && userInfo.isAdmin && (
+                      <>
+                        <hr />
+                        <NavDropdown.Item>Admin</NavDropdown.Item>
+                        <hr />
+                        <LinkContainer to="/admin/users">
+                          <NavDropdown.Item>Users</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to="/admin/products">
+                          <NavDropdown.Item>Products</NavDropdown.Item>
+                        </LinkContainer>
+                        <LinkContainer to="/admin/orders">
+                          <NavDropdown.Item>Orders</NavDropdown.Item>
+                        </LinkContainer>
+                      </>
+                    )}
+                  </NavDropdown>
+                ) : (
+                  <>
+                    <NavDropdown
+                      title={<i className="icon fas fa-user"></i>}
+                      id="userMenu"
+                    >
+                      <LinkContainer to="/signin">
+                        <NavDropdown.Item>Sign In</NavDropdown.Item>
+                      </LinkContainer>
+
+                      <LinkContainer to="/register">
+                        <NavDropdown.Item>Register</NavDropdown.Item>
+                      </LinkContainer>
+                    </NavDropdown>
+                  </>
+                )}
+                <LinkContainer to="/cart">
+                  <Nav.Link className="nav-link">
+                    <i className="icon fas fa-shopping-cart"></i>
+                    <sup>
+                      <Badge pill variant="success">
+                        {cartItems.length !== 0 &&
+                          cartItems.reduce(
+                            (acc, item) => acc + item.quantity,
+                            0
+                          )}
+                      </Badge>
+                    </sup>
+                  </Nav.Link>
                 </LinkContainer>
-              </NavDropdown>
-            )}
-          </Nav>
-          <LinkContainer to="/cart">
-            <Nav.Link className="nav-link mr-3">
-              <i className="icon fas fa-shopping-cart"></i>
-              <sup>
-                <Badge style={{ position: "absolute" }} pill variant="success">
-                  {cartItems.length !== 0 &&
-                    cartItems.reduce((acc, item) => acc + item.quantity, 0)}
-                </Badge>
-              </sup>
-            </Nav.Link>
-          </LinkContainer>
-          {/* </Navbar.Collapse> */}
-        </Container>
-      </Navbar>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
     </>
   );
 };
